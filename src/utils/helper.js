b975149b19ab3aca173b2302d4af45dedf8f74e2
Alan Baby <alan.baby@dbizsolution.com>
2024-10-01 13:45:13 +0530

    base url changed again

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,2 +1,2 @@
 // export const  BASE_URL='http://localhost:4500'
-export const  BASE_URL='https://my-article-app-server.onrender.com//'
+export const BASE_URL = 'https://my-article-app-server.onrender.com'
